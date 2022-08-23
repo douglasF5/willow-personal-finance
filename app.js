3cd2a113517c5a9fae4546246e86a7ba8d8ee90e
Douglas Ferreira <dfaferreira46@gmail.com>
2022-08-23 17:04:03 -0300

    set up express server

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-// app.get('/', (req, res) => {
-//     res.sendFile(`${__dirname}/public/index.html`);
-// });
-
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
