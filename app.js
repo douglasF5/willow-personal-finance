b07922a32a036230828d261c3fe2f3712de6dd29
Douglas Ferreira <dfaferreira46@gmail.com>
2022-08-31 15:36:50 -0300

    updatd server port

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv/config");
 const express = require('express')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+// const port = 3000;
 // const { createTransaction, getFinanceData } = require("./infra/transactions-repo");
 const { createTransaction, getFinanceData } = require("./infra/sql-transactions-repo");
 
